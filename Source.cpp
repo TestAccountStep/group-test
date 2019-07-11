b4bb0e14e5901e8b8954aac661a5fdf8e950dda2
Student Test <test@ex.ua>
2019-07-11 20:29:01 +0300

    Added text.

---
+++ Source.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Food
+{
+protected:
+	double price;
+	string name;
+public:
+	virtual string getName() = 0;
+	virtual double getPrice() = 0;
+};
+
+class Pizza: public Food
+{
+	
+public:
+	string getName() override
+	{
+		return "pizza";
+	}
+	double getPrice() override
+	{
+		return 99.99;
+	}
+};
+
+class DynamicWrapper: public Food
+{
+
+};
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+
+	auto pizza = new Pizza;
+
+	cout << "Name: " << pizza->getName() << "\nPrice: " << pizza->getPrice() << "\n";
+
+	system("pause");
+}
